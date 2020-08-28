00aedd02b404e58432e918c29777f8b8c17e2f02
Neil Stoker <neil.stoker@gmail.com>
2020-08-28 19:34:55 +0100

    Basic template loading and running

--- src/main.go
+++ src/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	godotenv.Load(".env")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatalf("PORT missing")
+	}
+
+	templates := populateTemplates()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requestedFile := r.URL.Path[1:]
+		t := templates.Lookup(requestedFile + ".html")
+		if t != nil {
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+
+	http.Handle("/img/", http.FileServer(http.Dir("public")))
+	http.Handle("/css/", http.FileServer(http.Dir("public")))
+
+	log.Printf("Listening on %s", port)
+	http.ListenAndServe(":"+port, nil)
+}
+
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+	const basePath = "templates"
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+
+	return result
 }
